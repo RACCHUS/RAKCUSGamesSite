a365fc4a1b8c7e7ef5df52cec241e46958963b64
RACCHUS <richardgurudeo@gmail.com>
2024-01-13 17:43:44 -0500

    Updated the homepage of the site
    Added site logo, favicon, github game links, and social links. Organized a simple landing page.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'RAKCUSGamesSite' title`, () => {
+  it(`should have the 'Welcome' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('RAKCUSGamesSite');
+    expect(app.title).toEqual('Welcome');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, RAKCUSGamesSite');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Welcome');
   });
 });
