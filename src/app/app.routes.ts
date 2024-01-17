40bfb10d46af00b8dd5f1151d783b0f4a4dfac26
RACCHUS <richardgurudeo@gmail.com>
2024-01-17 08:17:07 -0500

    Added route for Mythic Might
    This route will be used to create the future web version of Mythic Might.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { MythicMightComponent } from './mythic-might.component';
 
-export const routes: Routes = [];
+ export const routes: Routes = [
+  { path: 'mythic-might', component: MythicMightComponent },
+  
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
