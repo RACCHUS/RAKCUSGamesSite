fd25e163f6ec3f57e65d4c620c9faadf6f9d5d24
RACCHUS <richardgurudeo@gmail.com>
2024-01-17 08:18:23 -0500

    Added additional libraries and methods
    Added libraries to support routing and onclick events. Added methods for hovering over elements and onclick events.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,35 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, Router, RouterModule } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, RouterModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.scss'
 })
+
 export class AppComponent {
-  title = 'Welcome';
+  isHovering = false;
+  showPlayButton = false;
+  backgroundStyle = '';
+
+  constructor(private router: Router) { }
+
+  onMouseEnter() {
+    this.isHovering = true;
+  }
+
+  onMouseLeave() {
+    this.isHovering = false;
+  }
+
+  onMythicMightClick() {
+    this.backgroundStyle = 'linear-gradient(to right, #2c2d34, #467a5e)';
+    this.showPlayButton = true;
+    this.router.navigate(['/mythic-might']);
+  }
+
 }
