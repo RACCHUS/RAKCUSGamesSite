8f47e5739cae91c42a536ae175ae09724ed8f582
RACCHUS <richardgurudeo@gmail.com>
2024-01-18 21:59:43 -0500

    Added routes for Mythic Might game, About page, and Jotto game

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { MythicMightComponent } from './mythic-might.component';
+import { MythicMightComponent } from './mythic-might/mythic-might.component';
+import { JottoComponent } from './jotto/jotto.component';
+import { AboutComponent } from './about/about.component';
 
  export const routes: Routes = [
   { path: 'mythic-might', component: MythicMightComponent },
+  { path: 'jotto', component: JottoComponent },
+  { path: 'about', component: AboutComponent },
   
 ];
 
